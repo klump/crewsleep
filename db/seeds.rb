0510c46723af8ec02811c73c4509b9d6d4d4faa3
Patrik <voxxar@gmail.com>
2011-11-21 14:40:22 +0100

    some dev seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,11 @@
 
 #This is the setup for DreamHack Winter 2011
 
+Sleep::Section.delete_all
+Sleep::Alarm.delete_all
+Sleep::Place.delete_all
+Crew::Person.delete_all
+
 main = Sleep::Section.create(:name=>"Main", :valid_minutes=>[0,30])
 main.gen_row(1, 1..46)
 main.gen_row(2, 1..45)
@@ -27,3 +32,24 @@ snark.gen_row(5, 1..11)
 snark.gen_row(6, 1..3)
 snark.gen_row(7, 1..9)
 snark.gen_row(8, 1..10)
+
+#testdata
+if Rails.env == "development"
+  def random_place
+    s = Sleep::Section.all.to_a[rand(2)]
+    r = s.rows[rand(s.rows.length)]
+    r.places[rand(r.places.length)]
+  end
+  def random_name
+    (0..10).map{(rand('z'.ord-'a'.ord) + 'a'.ord).chr}.join("")
+  end
+  
+  10.times do
+    pl = Sleep::Section.all.to_a[rand(2)].rows
+    Crew::Person.create(:firstname=>random_name, :username=>random_name, :place => random_place)
+  end
+  persons = Crew::Person.all.to_a
+  100.times do
+    Sleep::Alarm.create({ :person => persons[rand(persons.length)], :time => Time.now + (rand(12)*3).hours })
+  end
+end
