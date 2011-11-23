76ac59f3e63def44ed64656898d4ca390ca3d59d
Patrik <voxxar@gmail.com>
2011-11-23 17:26:41 +0100

    date formatting

--- public/crew/list-controller.js
+++ public/crew/list-controller.js
@@ -1,3 +1,28 @@
+// Date.prototype.setISO8601 = function (string) {
+//     var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" +
+//         "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\.([0-9]+))?)?" +
+//         "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?";
+//     var d = string.match(new RegExp(regexp));
+// 
+//     var offset = 0;
+//     var date = new Date(d[1], 0, 1);
+// 
+//     if (d[3]) { date.setMonth(d[3] - 1); }
+//     if (d[5]) { date.setDate(d[5]); }
+//     if (d[7]) { date.setHours(d[7]); }
+//     if (d[8]) { date.setMinutes(d[8]); }
+//     if (d[10]) { date.setSeconds(d[10]); }
+//     if (d[12]) { date.setMilliseconds(Number("0." + d[12]) * 1000); }
+//     if (d[14]) {
+//         offset = (Number(d[16]) * 60) + Number(d[17]);
+//         offset *= ((d[15] == '-') ? 1 : -1);
+//     }
+// 
+//     offset -= date.getTimezoneOffset();
+//     time = (Number(date) + (offset * 60 * 1000));
+//     this.setTime(Number(time));
+// }
+
 var ListController = new Class({
 	initialize: function(listView, alarmList) {
 		this.listView = listView
@@ -35,8 +60,13 @@ var ListController = new Class({
 		console.log(activeAlarms)
 		this.alarmList.empty()
 		activeAlarms.each(function(time) {
+		  var t = new Date()
+		  t.setTime(Date.parse(time["time"]))
+		  var d = ["Sunday", "Monday", "Tuesday", "Wednesday", "Tusday", "Friday", "Saturday"][t.getDay()]
+		  var h = t.getHours()
+		  var m = t.getMinutes()
 			var timeHeader = new Element("h2", {
-				html: time["time"]
+				html: d + " " + h + ":" + m
 			})
 			timeHeader.inject(this.alarmList)
 
