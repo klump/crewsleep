e96d37fa631c46cca434868e252102d7a47243a7
Stefan Lindbohm <stefan@kolizz.com>
2012-06-14 04:08:56 +0200

    Cleanup

--- lib/cco/service.rb
+++ lib/cco/service.rb
@@ -3,39 +3,38 @@ require 'net/https'
 require 'uri'
 
 module Cco
+  class Service
 
-end
-class Cco::Service
+    def self.fetch_person(id_or_nick)
+      uri = URI.parse("https://crew.dreamhack.se/export/user?zion=raw&user="+id_or_nick.to_s)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(uri.request_uri)
+      request.basic_auth("ssv", "vss")
+      response = http.request(request)
+      response_object = PHP.unserialize(response.body)
 
-  def self.fetch_person(id_or_nick)
-    uri = URI.parse("https://crew.dreamhack.se/export/user?zion=raw&user="+id_or_nick.to_s)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    request = Net::HTTP::Get.new(uri.request_uri)
-    request.basic_auth("ssv", "vss")
-    response = http.request(request)
-    response_object = PHP.unserialize(response.body)
+      return nil if response_object == false
 
-    return nil if response_object == false
+      if (response_object.kind_of?(Array)) then
+      return nil if response_object.empty?
+        person_hash = response_object[0]
+      else
+      person_hash = response_object
+      end
 
-    if (response_object.kind_of?(Array)) then
-	  return nil if response_object.empty?
-      person_hash = response_object[0]
-    else
-	  person_hash = response_object
+      avatar_url = person_hash.has_key?("img") ? person_hash["img"] : nil
+
+    person = Crew::Person.create(
+      :username => person_hash["username"],
+      :firstname => person_hash["firstname"],
+      :lastname => person_hash["lastname"],
+      :cco_id => person_hash["uid"],
+      :avatar_url => avatar_url
+    )
+
+    return person
     end
 
-    avatar_url = person_hash.has_key?("img") ? person_hash["img"] : nil
-
-	person = Crew::Person.create(
-	  :username => person_hash["username"],
-	  :firstname => person_hash["firstname"],
-	  :lastname => person_hash["lastname"],
-	  :cco_id => person_hash["uid"],
-	  :avatar_url => avatar_url 
-	)
-
-	return person
   end
-
 end
